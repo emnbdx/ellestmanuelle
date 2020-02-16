239f4b48af6452ef8cf09b59a034ca62b7c79aca
Eddy <eddy.montus@gmail.com>
2020-02-17 00:30:41 +0100

    edit menu content, split some page, add contact page

---
+++ includes/contact-form.php
@@ -0,0 +1,67 @@
+<?php
+
+
+
+$string = file_get_contents("config.json");
+
+$option = json_decode($string);
+
+
+
+define("MAIL_HOST", $option->MAIL_HOST);
+
+define("MAIL_TITLE", $option->MAIL_TITLE);
+
+
+
+if (isset($_POST['name']))
+
+    $name = $_POST['name'];
+
+else
+
+    $name = "unknown";
+
+
+if (isset($_POST['email']))
+
+    $email = $_POST['email'];
+
+else
+
+    $email = "unknown";
+
+
+if (isset($_POST['message']))
+
+    $message = nl2br($_POST['message']);
+
+else
+
+    $message = "unknown";
+
+
+$from = $email;
+
+$message = '<b>Nom:</b>'.$name. '<br/><b>Email:</b>'.$email.'<br/><p>'.$message.'</p>';
+
+$headers = "From: $from\n";
+
+$headers .= "MIME-Version: 1.0\n";
+
+$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+
+if( mail(MAIL_HOST, MAIL_TITLE, $message, $headers) ) {
+
+    $serialized_data = '{"type":"success", "message":"Votre message a bien été envoyé"}';
+
+    echo $serialized_data;
+
+} else {
+
+    $serialized_data = '{"type":"danger", "message":"Erreur lors de l\'envoie du message, merci de reessayer plus tard"}';
+
+    echo $serialized_data;
+
+}
+
