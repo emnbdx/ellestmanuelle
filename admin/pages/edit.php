3e36874f27013f506e0e92d53c8ffa31359f9847
Eddy <eddy.montus@gmail.com>
2020-03-08 15:36:50 +0100

    improve rich editor on page edition

--- admin/pages/edit.php
+++ admin/pages/edit.php
@@ -39,7 +39,15 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" href="/vendor/codemirror/css/codemirror.css">
-    <link rel="stylesheet" href="/vendor/codemirror/theme/darcula.css">
+	<link rel="stylesheet" href="/vendor/codemirror/theme/darcula.css">
+	
+	<style>
+		#preview, .CodeMirror {
+			width: 100%;
+			height : 600px;
+		}
+	</style>
+
     <title>Ellestmanuelle | Administration créations</title>
   </head>
   <body>
@@ -55,22 +63,28 @@
 				<i class="fa fa-fw fa-plus-circle"></i> <strong>Edit Page</strong> 
 				<a href="index.php" class="float-right btn btn-dark btn-sm"><i class="fa fa-arrow-circle-left"></i> Browse Pages</a>
 			</div>
-			<div class="card-body">				
+			<div class="card-body">
 				<div class="col-12">
 					<form method="post" enctype="multipart/form-data">
 						<div class="form-group">
 							<label>Name</label>
 							<input type="text" name="name" id="name" class="form-control" value="<?php echo $page['name']; ?>" required disabled>
 						</div>
-						<div class="form-group">
-							<label>Content <span class="text-danger">*</span></label>
-							<textarea name="content" id="content" class="form-control" required><?php echo $page['content']; ?></textarea>
+						<div class="row">
+							<div class="col-6">
+								<div class="form-group">
+								<label>Content <span class="text-danger">*</span></label>
+								<textarea name="content" id="content" class="form-control" required><?php echo $page['content']; ?></textarea>
+								</div>
+							</div>
+							<div class="col-6">
+								<div class="form-group">
+									<label>Preview <span class="text-danger">*</span></label>
+									<br/>
+									<iframe src="" id="preview"></iframe>
+								</div>
+							</div>
 						</div>
-						<div class="form-group">
-							<label>Preview <span class="text-danger">*</span></label>
-							<br/>
-							<iframe src="" id="preview" width="100%"></iframe>
-						</div>						
 						<div class="form-group">
 							<input type="hidden" name="editId" id="editId" value="<?php echo $_REQUEST['editId']?>">
 							<button type="submit" name="submit" value="submit" id="submit" class="btn btn-primary"><i class="fa fa-fw fa-edit"></i> Update Page</button>
@@ -99,7 +113,8 @@
 		}
 		  
 		var cm = CodeMirror.fromTextArea(document.getElementById('content'), {
-    		lineNumbers: true,
+			lineNumbers: true,
+			lineWrapping: true,
 			mode: "htmlmixed",
 			theme: "darcula"
 		}).on("change", function(cm, change) {
