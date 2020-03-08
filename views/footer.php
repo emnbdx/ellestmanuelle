9a8c566a29f179c9308880bfe01748089f2a7029
Eddy <eddy.montus@gmail.com>
2020-03-08 15:49:09 +0100

    add linkedin link on footer

--- views/footer.php
+++ views/footer.php
@@ -5,21 +5,17 @@
 				<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 					<div class="footer-items footer-items-1">
 						<h4>Contact</h4>
-						<!-- <div class="contact">
-							<i class="zmdi zmdi-map"></i>
-							<span>No 40 Baria Sreet 133/2</span>
-						</div> -->
-						<div class="contact">
-							<i class="zmdi zmdi-phone"></i>
-							<span><a href="tel:+33600000000">+ (33) 06 00 00 00 00</a></span>
-						</div>
 						<div class="contact">
 							<i class="zmdi zmdi-email"></i>
-							<span>contact@ellestmanuelle.fr</span>
+							<span><a href="mailto:ontact@ellestmanuelle.fr" target="_blank">contact@ellestmanuelle.fr</a></span>
 						</div>
 						<div class="contact">
 							<i class="zmdi zmdi-globe"></i>
-							<span>www.ellestmanuelle.fr</span>
+							<span><a href="https://www.ellestmanuelle.fr" target="_blank">www.ellestmanuelle.fr</a></span>
+						</div>
+						<div class="contact">
+							<i class="zmdi zmdi-linkedin"></i>
+							<span><a href="https://fr.linkedin.com/in/ellestmanuelle" target="_blank">linkedin.com/ellestmanuelle</a></span>
 						</div>
 					</div>
 				</div>
