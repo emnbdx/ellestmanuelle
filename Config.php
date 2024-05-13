b938e88c53eb8318f027834d48a0b89997618ecf
emnbdx <eddy.montus@gmail.com>
2024-05-14 00:39:41 +0200

    add env var

--- Config.php
+++ Config.php
@@ -1,23 +1,23 @@
 <?php
     class Config {
         // DB
-        static $DBURL = "127.0.0.1";
-        static $DBNAME = "ellestmanuelle";
-        static $DBUSER = "root";
-        static $DBPASSWORD = null;
+        static $DBURL = getenv('DATABASE_HOST');
+        static $DBNAME = getenv('DATABASE_NAME');
+        static $DBUSER = getenv('DATABASE_USERNAME');
+        static $DBPASSWORD = getenv('DATABASE_PASSWORD');
     
         // FILE
         static $UPLOADFOLDER = __DIR__ . "/images/uploads/";
-        static $UPLOADMAXSIZE = 5000000;
+        static $UPLOADMAXSIZE = 10000000;
 
         // MAIL
         static $MAIL_HOST = "ssl0.ovh.net";
         static $MAIL_PORT = 465;
         static $MAIL_LOGIN = "contact@ellestmanuelle.fr";
-        static $MAIL_PASSWORD = "";
+        static $MAIL_PASSWORD = getenv('MAIL_PASSWORD');
 
         // ADMIN
-        static $ADMIN_USER = "user";
-        static $ADMIN_PASSWORD = "password";
+        static $ADMIN_USER = "emmanuelle";
+        static $ADMIN_PASSWORD = getenv('ADMIN_PASSWORD');
     }
 ?>
